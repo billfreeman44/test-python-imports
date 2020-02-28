73d55843b4e6d38d9d137d1651d54573d221c35a
Bill Freeman <billfreeman44@yahoo.com>
2020-02-28 09:03:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='test_python_imports',
+   version='1.0',
+   description='A useful module',
+   author='billfreeman44',
+   author_email='foomail@foo.com',
+   packages=['test_python_imports']
+)
+
 print('test import worked')
